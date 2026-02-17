c11e1dc975c930ddbee1718fe735a2deb01f185b
valerioitsg <itsg.vlopez@gmail.com>
2026-02-17 09:54:22 -0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import ITSGWebsite from "./ITSGWebsite.jsx";
-import "./index.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import ITSGWebsite from './ITSGWebsite.jsx'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  React.createElement(React.StrictMode, null,
-    React.createElement(ITSGWebsite, null)
-  )
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <ITSGWebsite />
+  </StrictMode>,
+)
